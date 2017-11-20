f20f20a833e753f4d2191f63f3b960d1ac9e2d4c
dianamariasilva <diana.silva.v94@gmail.com>
2017-11-20 00:58:10 -0500

    añadiendo las carpetas correspondientes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './Login';
+import Register from './Register';
+
+import {Provider} from 'redux-zero/react'
+import {HashRouter, Route, Switch} from 'react-router-dom'
+import store from './store'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Index = () =>  (
+   <Provider store={store}> 
+      <HashRouter>
+         <Switch>
+            <Route name="login" exact path = "/login" component = {Login}/>
+            <Route name="register"  exact path = "/register" component = {Register}/>         
+            <Route name="home"  exact path = "/home" component = {App}/>
+            <Route name="trello"  exact path = "/trello" component = {TrelloApp}/>
+         </Switch>
+      </HashRouter>
+   </Provider>
+)
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
+
